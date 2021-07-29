932c4a86720abcf1adb7942ee206c871553e01d9
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-29 14:50:27 +0300

    Products Manager

--- product-Manager/server/routes/product.routes.js
+++ product-Manager/server/routes/product.routes.js
@@ -3,7 +3,7 @@ const ProductController =require("../controllers/product.controller");
 module.exports=app=>{
     app.get('/api', ProductController.index);
     app.get("/api/products",ProductController.findAllProduct);
-    app.get("/api/Products/:id",ProductController.findOneProduct);
+    app.get("/api/products/:id",ProductController.findOneProduct);
     app.put("/api/products/update/:id",ProductController.updateExistingProduct);
     app.post("/api/products/new",ProductController.creatProduct);
     app.delete("/api/products/delete/:id",ProductController.deleteProduct);
